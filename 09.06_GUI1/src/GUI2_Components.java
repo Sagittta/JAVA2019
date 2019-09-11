8ca2f36b913566ae720e389c13fb4915ac8f0a8a
Sagittta <js1verj@gmail.com>
2019-09-11 10:06:44 +0900

    Components, label, button, checkbox, radiobutton + buttonGroup, textfield, textarea, combobox + addItem

---
+++ 09.06_GUI1/src/GUI2_Components.java
@@ -0,0 +1,43 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class GUI2_Components {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Components");
+        JPanel panel = new JPanel();
+        JLabel lb0 = new JLabel("JLabel");
+        JButton bt0 = new JButton("JButton");
+        JCheckBox cb0 = new JCheckBox("JCheckBox0");
+        JCheckBox cb1 = new JCheckBox("JCheckBox1");
+        JRadioButton rb0 = new JRadioButton("미성년자");
+        JRadioButton rb1 = new JRadioButton("성인");
+        ButtonGroup bg = new ButtonGroup();
+        bg.add(rb0);
+        bg.add(rb1);
+        JTextField tf0 = new JTextField("JTextField");
+//        JTextArea ta0 = new JTextArea("JTextArea");
+        JTextArea ta0 = new JTextArea(2, 10);
+        String[] fruits = {"apple", "banana", "kiwi", "mango", "pear", "peach"};
+        JComboBox cbox0 = new JComboBox(fruits);
+        JComboBox<String> cbox1 = new JComboBox<>(fruits);
+        cbox1.addItem("blueberry");
+        cbox1.addItem("strawberry");
+
+        panel.add(lb0);
+        panel.add(bt0);
+        panel.add(cb0);
+        panel.add(cb1);
+        panel.add(rb0);
+        panel.add(rb1);
+        panel.add(tf0);
+        panel.add(ta0);
+        panel.add(cbox0);
+        panel.add(cbox1);
+        frame.add(panel);
+
+        frame.setPreferredSize(new Dimension(600, 400));
+        frame.pack();
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
